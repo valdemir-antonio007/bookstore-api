af1b88d69a521f33b99955e70bb2451da526faf7
Valdemir Antonio <gerente.decorarte@hotmail.com>
2023-08-06 23:22:51 -0300

    Instancia de forma automatica da base de dados

--- src/main/java/com/valdemir/bookstore/BookstoreApplication.java
+++ src/main/java/com/valdemir/bookstore/BookstoreApplication.java
@@ -1,13 +1,41 @@
 package com.valdemir.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.valdemir.bookstore.domain.Categoria;
+import com.valdemir.bookstore.domain.Livro;
+import com.valdemir.bookstore.repositories.CategoriaRepository;
+import com.valdemir.bookstore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livros de TI");
+		
+		Livro l1 = new Livro(null, "Celan Code", "Robert Martin", "Lorem Ipsum", cat1 );
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
 	}
 
 }
